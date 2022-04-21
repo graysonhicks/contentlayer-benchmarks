3775b01c082cbae0625ffc29ae5b62de0bd53ea3
Ward Peeters <ward@coding-tech.com>
2022-04-21 20:00:36 +0200

    add schema customization

---
+++ gatsby/src/gatsby-node.ts
@@ -0,0 +1,15 @@
+import type { GatsbyNode } from "gatsby";
+
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
+  ({ actions }) => {
+    actions.createTypes(`
+    type MarkdownRemark implements Node @dontInfer {
+      frontmatter: Frontmatter
+      html: String!
+    }
+
+    type Frontmatter {
+      title: String!
+    }
+  `);
+  };
